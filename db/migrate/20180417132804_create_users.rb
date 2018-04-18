08b7515bc77d39916ab18d92e3042ae954be44b6
Alex Viñas <alex.vinyas@gmail.com>
2018-04-18 22:30:01 +0200

    Added profile view basic

--- db/migrate/20180417132804_create_users.rb
+++ db/migrate/20180417132804_create_users.rb
@@ -6,7 +6,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :oauth_token
       t.datetime :oauth_expires_at
-
+      t.string :email
       t.timestamps
     end
   end
