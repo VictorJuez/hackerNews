62b491fed4d59af87e24ab4d27018a4652894257
Alex Viñas <alex.vinyas@gmail.com>
2018-04-23 13:51:09 +0200

    Updated db

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180417132804) do
 
-  create_table "submissions", force: :cascade do |t|
-    t.string "title"
-    t.string "url"
-    t.string "text"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_submissions_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "provider"
     t.string "uid"
@@ -33,6 +23,16 @@ ActiveRecord::Schema.define(version: 20180417132804) do
     t.string "about"
     t.integer "karma"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "submissions", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.string "text"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_submissions_on_user_id"
   end
 
 end
