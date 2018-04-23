e6871caf61c72149f1f7ad77df6cc68b0db422ff
Alex Viñas <alex.vinyas@gmail.com>
2018-04-23 14:09:55 +0200

    prova heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-	def self.from_omniauth(auth)
+	has_many :submissions
+
+  def self.from_omniauth(auth)
     	where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       	user.provider = auth.provider
       	user.uid = auth.uid
--- db/schema.rb
+++ db/schema.rb
@@ -19,10 +19,8 @@ ActiveRecord::Schema.define(version: 20180417132804) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_submissions_on_user_id"
   end
-
-  add_index "submissions", ["user_id"], name: "index_submissions_on_user_id"
-  #add_index "submissions", ["user_id"], name: "index_submissions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string "provider"
