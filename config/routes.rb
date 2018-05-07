7066dff1b5fbfa6f71ecce35d669423c25d3a5b3
Alex Viñas <alex.vinyas@gmail.com>
2018-05-07 14:13:48 +0200

    revert "Merge branch 'Development'"
    This reverts commit 9fa18d14d466f97042ad6b8c3e9fd4aec4b852b4, reversing
    changes made to cde0f4bae485c611d437ab37db2a623cf4212b62.

--- config/routes.rb
+++ config/routes.rb
@@ -1,64 +1,35 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
 
-#API
-  namespace 'api' do
-    namespace 'v1' do
+  get 'sessions/destroy'
 
-      #submissions
-      get '/submissions' => 'submissions#index'
-      get '/submissions/newest' => 'submissions#newest'
-      get '/submissions/ask' => 'submissions#ask'
-      get '/submissions/new' => 'submissions#new'
-      get '/submissions/:id/' => 'submissions#show'
-      post '/submissions' => 'submissions#create'
-      post '/submissions/:id/vote' => 'submissions#vote'
-      post '/submissions/:id/unvote' => 'submissions#unvote'
-
-      #comments
-      get 'threads' => 'comments#threads'
-      get '/submissions/:id/comments' => 'comments#submission_comments'
-      post 'comments/:id/vote' => 'comments#vote'
-      post 'comments/:id/unvote' => 'comments#unvote'
-      put '/comments/:id/update' => 'comments#update'
-    end
-  end
-
-
-  #NO API
   resources :submissions
   resources :users
   resources :comments
   resources :replies
 
-  #sessions
-  resources :sessions, only: [:create, :destroy]
-  get 'sessions/create'
-  get 'sessions/destroy'
+  
+  get 'newest' => 'submissions#newest'
+  get 'ask' => 'submissions#ask'
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
-  
-  #submissions
-  get '/submissions' => 'submissions#index'
-  get '/submissions/newest' => 'submissions#newest'
-  get '/submissions/ask' => 'submissions#ask'
-  get '/submissions/new' => 'submissions#new'
-  get '/submissions/:id/' => 'submissions#show'
-  post '/submissions' => 'submissions#create'
-  post '/submissions/:id/vote' => 'submissions#vote'
-  post '/submissions/:id/unvote' => 'submissions#unvote'
-
-  #comments
   get 'threads' => 'comments#threads'
-  get '/submissions/:id/comments' => 'comments#submission_comments'
+
+  post 'submissions/:id/vote' => 'submissions#vote'
+  post 'submissions/:id/unvote' => 'submissions#unvote'
   post 'comments/:id/vote' => 'comments#vote'
   post 'comments/:id/unvote' => 'comments#unvote'
-
-  #replies
   post 'replies/:id/vote' => 'replies#vote'
   post 'replies/:id/unvote' => 'replies#unvote'
+
   get 'comments/:id/new_reply' => 'comments#new_reply'
+
+  get '/submissions/:id/comments' => 'comments#submission_comments'
   get '/comments/:id/replies' => 'replies#comment_replies'
+    
+  resources :sessions, only: [:create, :destroy]
+
   
   root :to => "submissions#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
