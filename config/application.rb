65be23f8f0c2cfb6965c96369d4a0c7b6904926f
Sergi <saguilar.anguera@gmail.com>
2018-05-08 23:01:43 +0200

    Added put to cors

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module PracticaAsw
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :put]
       end
     end
   end
