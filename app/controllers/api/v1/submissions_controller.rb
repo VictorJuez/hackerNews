a58f90e3671c574011b371e8d7ecf04e8ec9cd75
Aaron Jimenez Garcia <aaronjimenezgarcia@gmail.com>
2018-05-10 15:23:47 +0200

    JSON submissions updated

--- app/controllers/api/v1/submissions_controller.rb
+++ app/controllers/api/v1/submissions_controller.rb
@@ -3,35 +3,125 @@ module Api
 		class SubmissionsController < ApplicationController
 			skip_before_action :verify_authenticity_token
 
+			def allJson(submissions)
+				response = []
+				submissions.each do |submission|
+					if submission.url != ""
+						subResponse = {
+							id: submission.id,
+							title: submission.title,
+							url: submission.url,
+							user_creator_id: submission.user_id,
+							created_at: submission.created_at,
+							votes: submission.cached_votes_total
+						}
+						response.push(subResponse)
+					else
+						subResponse = {
+							id: submission.id,
+							title: submission.title,
+							text: submission.text,
+							user_creator_id: submission.user_id,
+							created_at: submission.created_at,
+							votes: submission.cached_votes_total
+						}
+						response.push(subResponse)
+					end
+				end
+				return response
+			end
+
 			def all
 				submissions = Submission.all.order(:cached_votes_total=> :desc)
-				render json: {status: 'SUCCESS', message: 'URL submissions', data: submissions}, status: :ok				
+				response = allJson(submissions)
+				render json: {status: 'SUCCESS', message: 'URL submissions', data: response}, status: :ok				
+			end
+
+			def urlJson(submissions)
+				response = []
+				submissions.each do |submission|
+					subResponse = {
+						id: submission.id,
+						title: submission.title,
+						url: submission.url,
+						user_creator_id: submission.user_id,
+						created_at: submission.created_at,
+						votes: submission.cached_votes_total
+					}
+					response.push(subResponse)
+				end
+				return response
 			end
 
 			def url
 				submissions = Submission.all.where.not(url:"").order("created_at DESC")
-				render json: {status: 'SUCCESS', message: 'URL submissions', data: submissions}, status: :ok				
+				response = urlJson(submissions)
+				render json: {status: 'SUCCESS', message: 'URL submissions', data: response}, status: :ok				
+			end
+
+			def askJson(submissions)
+				response = []
+				submissions.each do |submission|
+					subResponse = {
+						id: submission.id,
+						title: submission.title,
+						text: submission.text,
+						user_creator_id: submission.user_id,
+						created_at: submission.created_at,
+						votes: submission.cached_votes_total
+					}
+					response.push(subResponse)
+				end
+				return response
 			end
 
 			def ask
 				submissions = Submission.all.where(url:"").order("created_at DESC")
-				render json: {status: 'SUCCESS', message: 'ASK submissions', data: submissions}, status: :ok
+				response = askJson(submissions)
+				render json: {status: 'SUCCESS', message: 'ASK submissions', data: response}, status: :ok
+			end
+
+			def showJson(submission)
+				response = []
+				if submission.url != ""
+					subResponse = {
+						id: submission.id,
+						title: submission.title,
+						url: submission.url,
+						user_creator_id: submission.user_id,
+						created_at: submission.created_at,
+						votes: submission.cached_votes_total
+					}
+					response.push(subResponse)
+				else
+					subResponse = {
+						id: submission.id,
+						title: submission.title,
+						text: submission.text,
+						user_creator_id: submission.user_id,
+						created_at: submission.created_at,
+						votes: submission.cached_votes_total
+					}
+					response.push(subResponse)
+				end
+				return response
 			end
 
 			def show
 				submission = Submission.where("id = ? ", params[:id])
 				if submission.empty?
-					render json: {status: 'ERROR', message: 'Submission does not exist', data: {}}, status: :unprocessable_entity
+					render json: {status: 'ERROR', message: 'Submission does not exist', data: {}}, :status => 404
 				else
 					submission = Submission.find(params[:id])
-					render json: {status: 'SUCCESS', message: 'Submission', data: submission}, status: :ok
+					response = showJson(submission)
+					render json: {status: 'SUCCESS', message: 'Submission', data: response}, status: :ok
 				end
 			end
 
 			def vote
 				submission = Submission.where("id = ?", params[:id])
 				if submission.empty?
-					render json: {status: 'ERROR', message: 'Submission does not exist', data: {}}, status: :unprocessable_entity
+					render json: {status: 'ERROR', message: 'Submission does not exist', data: {}}, :status => 404
 				else
 					submission = Submission.find(params[:id])
 					begin
@@ -53,7 +143,7 @@ module Api
 						end
 					else 
 						render json: {status: 'ERROR', message: 'Error in authenticity', data: submission.errors}, 
-								status: :unprocessable_entity
+								:status => 403
 					end
 				end
 			end
@@ -88,7 +178,7 @@ module Api
 						end
 					else
 						render json: {status: 'ERROR', message: 'Error in authenticity', data: submission.errors}, 
-								status: :unprocessable_entity
+								:status => 403
 					end
 				end
 			end
@@ -127,10 +217,11 @@ module Api
 					        subuser.karma = subuser.karma + 1
 							if submission.save && subuser.save
 								submission.vote_by :voter => current_user
-								render json: {status: 'SUCCESS', message: 'Submission saved', data: submission}, status: :ok
+								response = showJson(submission)
+								render json: {status: 'SUCCESS', message: 'Submission saved', data: response}, status: :ok
 							else
 								render json: {status: 'ERROR', message: 'Submission not saved', data: submission.errors}, 
-								status: :unprocessable_entity
+								:status => 500
 							end	
 						else
 							render json: {status: 'ERROR', message: 'Exists a submission with this url', data: submission.errors}, 
@@ -138,11 +229,11 @@ module Api
 						end
 					else
 						render json: {status: 'ERROR', message: 'Error in parameters', data: submission.errors}, 
-							status: :unprocessable_entity
+							:status => 417
 					end
 				else
 					render json: {status: 'ERROR', message: 'Error in authenticity', data: submission.errors}, 
-							status: :unprocessable_entity
+							:status => 403
 				end
 			end
 
